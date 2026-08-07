600bbaebea911e47bc25e1291244db0c15ef2195
Fayez-cpu <100725949@unimail.derby.ac.uk>
2026-08-07 21:22:25 +0100

    same site secure

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -7,8 +7,8 @@ export const generateToken = (userid, res) => {
     })
     res.cookie("jwt", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        secure: true,
+        sameSite: "none",
         maxAge: (1000 * 60 * 60 * 24) * 7
     });
     return token;
