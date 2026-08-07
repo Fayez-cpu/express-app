3f928d06d7449ebe2c115fac06555dfa17ed228d
Fayez-cpu <100725949@unimail.derby.ac.uk>
2026-08-07 20:58:29 +0100

    added console logs

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -6,15 +6,18 @@ export const authMiddleware = async(req, res, next) => {
     let token
     if (req.cookies?.jwt){
         token = req.cookies.jwt
+        console.log("token found in cookies")
     }
+    
     else{
         return res.status(401).json({error: "Not authorized"})
     }
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        console.log("decoded token", decoded)
         const user = await db.oneOrNone('SELECT name,email,id from users WHERE id = $1', [decoded.id])
         if (!user){
-            return res.status(401).json({error: "Not authorized"})
+            return res.status(401).json({error: "Not authorized, error"})
         }
         req.user = user
         next()
