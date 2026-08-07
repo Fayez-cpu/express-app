42d739b32af2f0f531b021b165814b5d769c70e6
Fayez-cpu <100725949@unimail.derby.ac.uk>
2026-08-07 19:09:30 +0100

    protected route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express'
 import { getAllData } from '../controllers/getAllData.js'
 import { getDataByPathParams } from '../controllers/getDataByPathParams.js'
-
+import {authMiddleware} from '../middleware/authMiddleware.js'
 export const apiRouter = express.Router()
 
-apiRouter.get('/', getAllData)
+apiRouter.get('/', authMiddleware, getAllData)
 
-apiRouter.get('/:field/:term', getDataByPathParams)
+apiRouter.get('/:field/:term', authMiddleware, getDataByPathParams)
