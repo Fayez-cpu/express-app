d7bd1e96f05eb15c1ae2c897254de16c3ac722f7
Fayez-cpu <100725949@unimail.derby.ac.uk>
2026-08-07 22:06:19 +0100

    auth fix

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -5,10 +5,11 @@ export const generateToken = (userid, res) => {
     const token = jwt.sign(payload, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRY || "7d",
     })
+    const isProduction = process.env.NODE_ENV === "production"
     res.cookie("jwt", token, {
         httpOnly: true,
-        secure: true,
-        sameSite: "none",
+        secure: isProduction,
+        sameSite: isProduction ? "none" : "lax",
         maxAge: (1000 * 60 * 60 * 24) * 7
     });
     return token;
